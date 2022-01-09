b3672ec2603ebbf479b11a00780bb3308ba5900a
Arya-Jain <89778433+Arya-Jain@users.noreply.github.com>
2022-01-09 12:12:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 song1 = "";
 song2 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristY = 0;
+rightWristY = 0;
+
 function preload(){
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3");
@@ -12,12 +18,31 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
+
+function modelLoaded()
+{
+	console.log("model is intialised");
+}
+  
 
 function draw(){
     image(video, 0, 0, 500, 400);
 }
 
-function play(){
- song.play();
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        rightWristY = results[0].pose.rightWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("the right wrist x = " + rightWristX + " y = " + rightWristY);
+        leftWristY = results[0].pose.leftWrist.y;
+        lefttWristX = results[0].pose.leftWrist.x;
+        console.log("the keft wrist x = " + leftWristX + " y = " + leftWristY);
+    }
 }
+
